7210e1ab9d6f300b47eceb9202fff1053e69ee15
Jonathan <160854949+CamzieAmepzo@users.noreply.github.com>
2025-04-08 18:13:45 +0700

    .

--- cuaca/script.js
+++ cuaca/script.js
@@ -1,20 +1,21 @@
-fetch('https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=16.71.18.1001')
-.then(response => response.json())
+// akses API desa kelurahan
+fetch("https://24252-if21-pw1-omega.vercel.app/javascript/wilayah.json")
+.then( response => response.json() )
 .then( data => {
     console.log(data);
     data.forEach( (item) => {
-        document.getElementById("list-desa").innerHTML +=  
-     
-    });
+        document.getElementById("list-desa").innerHTML += `<li onclick='detail("${item.kode}")'> ${item.kode} ${item.nama } </li>`
+    })
+} )
 
-})
-
-fuction detail (kode){
+function detail(kode) {
     console.log(kode);
-
-
-
-
-
+    // akses API cuaca BMKG berdasarkan kode wilayah desa/kelurahan
+    fetch(`https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4=${kode}`)
+    .then( response => response.json() )
+    .then( data => {
+        console.log(data);
+        document.getElementById('desa').innerHTML = data.lokasi.desa;
+        document.getElementById('kecamatan').innerHTML = data.lokasi.kecamatan;
+    } )
 }
-
